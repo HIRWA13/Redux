3cd97040abfbe64ef06af52cd9fee038d8344d91
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-06 15:41:02 +0200

    feat: create Redux action

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('from index.js');
+const BUY_CAKE = "BUY_CAKE"; // action name
+
+// create an action creator -> function that returns an action
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "first Redux action",
+  };
+}
