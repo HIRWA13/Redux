f2709ec834e878e971e9408a37dcf8d1e05f42de
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-06 16:23:46 +0200

    feat: create a reducer

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function buyCake() {
     info: "first Redux action",
   };
 }
+
+const initialState = {
+    numberOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE: {
+            return {
+                ...state,  // make a copy of the state object in case it con
+                numberOfCakes: state.numberOfCakes - 1
+            }
+        }
+        default: return state;
+    }
+}
